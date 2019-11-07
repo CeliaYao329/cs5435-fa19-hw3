1929f3ab46cafd8c5b289c553207f437249bc609
CeliaYao329 <ycelia29@gmail.com>
2019-11-06 23:36:43 -0500

    replace cbc mode

--- app/api/encr_decr.py
+++ app/api/encr_decr.py
@@ -1,6 +1,7 @@
 import os
 from cryptography.hazmat.primitives import hashes, padding, ciphers
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 import base64
 import binascii
 
@@ -16,36 +17,35 @@ class Encryption(object):
     def __init__(self, in_key=None):
         self._backend = default_backend()
         self._block_size_bytes = int(ciphers.algorithms.AES.block_size/8)
-        print("Encryption block size: ", self._block_size_bytes)
+        self._aad = b"sy684"
         if in_key is None:
-            self._key = os.urandom(self._block_size_bytes)
+            self._key = AESGCM.generate_key(bit_length=128)
         else:
             self._key = in_key
+        self._aesgcm = AESGCM(self._key)
 
     def encrypt(self, msg):
-        padder = padding.PKCS7(ciphers.algorithms.AES.block_size).padder()
-        padded_msg = padder.update(msg) + padder.finalize()
+        # padder = padding.PKCS7(ciphers.algorithms.AES.block_size).padder()
+        # padded_msg = padder.update(msg) + padder.finalize()
         iv = os.urandom(self._block_size_bytes)
-        encryptor = ciphers.Cipher(ciphers.algorithms.AES(self._key),
-                                   ciphers.modes.GCM(iv),
-                                   self._backend).encryptor()
+        _ciphertext = iv + self._aesgcm.encrypt(iv, msg, self._aad)
+        # encryptor = ciphers.Cipher(ciphers.algorithms.AES(self._key),
+        #                            ciphers.modes.GCM(iv),
+        #                            self._backend).encryptor()
         # TODO test
-        _ciphertext = iv + encryptor.update(padded_msg) + encryptor.finalize()
-        # _ciphertext = encryptor.update(padded_msg) + encryptor.finalize()
-        # self.iv = iv
+        # _ciphertext = iv + encryptor.update(msg) + encryptor.finalize()
         return _ciphertext
     
     def decrypt(self, ctx):
-        # TODO test
         iv, ctx = ctx[:self._block_size_bytes], ctx[self._block_size_bytes:]
-        # iv = self.iv
-        unpadder = padding.PKCS7(ciphers.algorithms.AES.block_size).unpadder()
-        decryptor = ciphers.Cipher(ciphers.algorithms.AES(self._key),
-                                   ciphers.modes.GCM(iv),
-                                   self._backend).decryptor()
-        padded_msg = decryptor.update(ctx) + decryptor.finalize()
+        msg = self._aesgcm.decrypt(iv, ctx, self._aad)
+        # unpadder = padding.PKCS7(ciphers.algorithms.AES.block_size).unpadder()
+        # decryptor = ciphers.Cipher(ciphers.algorithms.AES(self._key),
+        #                            ciphers.modes.GCM(iv),
+        #                            self._backend).decryptor()
+        # msg = decryptor.update(ctx) + decryptor.finalize()
         try:
-            msg = unpadder.update(padded_msg) + unpadder.finalize()
+            # msg = unpadder.update(padded_msg) + unpadder.finalize()
             return msg  # Successful decryption
         except ValueError:
             return False  # Error!!
